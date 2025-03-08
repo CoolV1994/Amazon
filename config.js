e311cb4ed7ec57a58d4fb34b29984cd1b8eea4c5
Vinnie <vmatronia@gmail.com>
2025-03-08 05:20:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,22 @@
+/* Amazon Link */
 
+// Base URL
 var URL_SITE = "https://coolv1994.github.io/Amazon";
 
+// Default Tag
 var ID_TAG = "gawdtech-20";
 
+// HTML ID: Redirect
+var SECTION_REDIRECT = "redirect";
+var URL_REDIRECT = "url-redirect";
 
+// HTML ID: Link
 var SECTION_LINK= "link";
 
-var SECTION_REDIRECT = "redirect";
+// HTML ID: Affiliate
 var SECTION_NEW = "new";
-var SECTION_OLD = "old";
-
-var URL_REDIRECT = "url-redirect";
 var URL_NEW = "url-new";
+
+// HTML ID: Amazon
+var SECTION_OLD = "old";
 var URL_OLD = "url-old";
